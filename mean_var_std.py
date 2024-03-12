8f90bc951217a6c6fca42dbc34acd823200f7139
Rigling, Heiko <Heiko.Rigling@sybit.de>
2024-03-12 15:01:48 +0100

    solution for exercise 1

--- mean_var_std.py
+++ mean_var_std.py
@@ -1,8 +1,24 @@
 import numpy as np
 
 def calculate(list):
+    if len(list) != 9:
+        raise ValueError('List must contain nine numbers.')
 
+    matrix = np.reshape(list, (3, 3))
 
-
-
+    calculations = {
+        'mean': execute_by_axis_and_flatten(matrix.mean),
+        'variance': execute_by_axis_and_flatten(matrix.var),
+        'standard deviation': execute_by_axis_and_flatten(matrix.std),
+        'max': execute_by_axis_and_flatten(matrix.max),
+        'min': execute_by_axis_and_flatten(matrix.min),
+        'sum': execute_by_axis_and_flatten(matrix.sum)
+    }
+    # print(list)
+    # print(matrix)
+    # print(calculations)
     return calculations
+
+
+def execute_by_axis_and_flatten(function):
+    return [function(axis=0).tolist(), function(axis=1).tolist(), function()]
